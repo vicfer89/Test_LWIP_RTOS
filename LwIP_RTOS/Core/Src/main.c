d70ff0f63a9a6ddb68173298b3b0493137d6b929
vicfer89 <vicfer89@gmail.com>
2023-02-12 11:22:26 +0100

    Actualiza FREE RTOS a una versión más moderna y actualiza timers.

--- LwIP_RTOS/Core/Src/main.c
+++ LwIP_RTOS/Core/Src/main.c
@@ -53,7 +53,6 @@ PCD_HandleTypeDef hpcd_USB_OTG_FS;
 osThreadId defaultTaskHandle;
 osThreadId LEDTaskHandle;
 osThreadId LD2TaskHandle;
-
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -453,6 +452,27 @@ void LD2Task_Init(void const * argument)
 	  osDelay(250);
   }
   /* USER CODE END LD2Task_Init */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM14 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM14) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
 }
 
 /**
